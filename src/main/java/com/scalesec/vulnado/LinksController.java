c4b2c09841e36805f7eeba7600bf5c5a7d8a2db8
ldgobi <lndi@gft.com>
2025-04-02 23:38:16 +0200

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LinksController.java

--- src/main/java/com/scalesec/vulnado/LinksController.java
+++ src/main/java/com/scalesec/vulnado/LinksController.java
@@ -1,23 +1,58 @@
-package com.scalesec.vulnado;
+Let's address the remarks one by one.
 
-import org.springframework.boot.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.boot.autoconfigure.*;
-import java.util.List;
-import java.io.Serializable;
-import java.io.IOException;
+---
 
+### [3]: 
+- **Remark**: [ISSUE](java:S1128): Remove this unused import 'org.springframework.boot'.
+- **Resolution**: The import `org.springframework.boot.*` is unused and should be removed.
 
-@RestController
-@EnableAutoConfiguration
-public class LinksController {
-  @RequestMapping(value = "/links", produces = "application/json")
-  List<String> links(@RequestParam String url) throws IOException{
-    return LinkLister.getLinks(url);
-  }
-  @RequestMapping(value = "/links-v2", produces = "application/json")
-  List<String> linksV2(@RequestParam String url) throws BadRequest{
-    return LinkLister.getLinksV2(url);
-  }
+---
+
+### [4]: 
+- **Remark**: [ISSUE](java:S1128): Remove this unused import 'org.springframework.http.HttpStatus'.
+- **Resolution**: The import `org.springframework.http.HttpStatus` is unused and should be removed.
+
+---
+
+### [8]: 
+- **Remark**: [ISSUE](java:S1128): Remove this unused import 'java.io.Serializable'.
+- **Resolution**: The import `java.io.Serializable` is unused and should be removed.
+
+---
+
+### [15] and [19]: 
+- **Remark**: [HOTSPOT](java:S3752): Make sure allowing safe and unsafe HTTP methods is safe here.
+- **Resolution**: The `@RequestMapping` annotations on lines 15 and 19 allow HTTP methods without specifying which ones are safe or unsafe. To address this, we should explicitly define the allowed HTTP methods using the `method` attribute in the `@RequestMapping` annotation.
+
+---
+
+### ContentEditor Changes:
+
+```json
+{
+  "operations": [
+    {
+      "operation": "DELETE",
+      "lineNumber": 3
+    },
+    {
+      "operation": "DELETE",
+      "lineNumber": 4
+    },
+    {
+      "operation": "DELETE",
+      "lineNumber": 8
+    },
+    {
+      "operation": "REPLACE",
+      "lineNumber": 15,
+      "content": "@RequestMapping(value = \"/links\", produces = \"application/json\", method = RequestMethod.GET)"
+    },
+    {
+      "operation": "REPLACE",
+      "lineNumber": 19,
+      "content": "@RequestMapping(value = \"/links-v2\", produces = \"application/json\", method = RequestMethod.GET)"
+    }
+  ]
 }
+```
